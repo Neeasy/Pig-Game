7de688872fb9b53b5964d0cd9f1381d12f81df72
NeeasyPC <nizerdota@yandex.ru>
2021-02-25 09:40:10 +0500

    final version

--- script.js
+++ script.js
@@ -10,17 +10,28 @@ const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
 
-console.log(score0El, score1El, diceEl);
+let scores, currentScore, activePlayer, playing;
+
 //Начальные значения;
-score0El.textContent = 0;
-score1El.textContent = 0;
-diceEl.classList.add('hidden');
 
-const scores = [0, 0];
-let currentScore = 0;
-let activePlayer = 0;
-let playing = true;
+const init = function () {
+  scores = [0, 0];
+  currentScore = 0;
+  activePlayer = 0;
+  playing = true;
 
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+  current0El.textContent = 0;
+  current1El.textContent = 0;
+
+  diceEl.classList.add('hidden');
+  player0El.classList.remove('player--winner');
+  player1El.classList.remove('player-winner');
+  player0El.classList.add('player--active');
+  player1El.classList.remove('player--active');
+};
+init();
 const switchPlayer = function () {
   document.getElementById(`current--${activePlayer}`).textContent = 0;
   currentScore = 0;
@@ -57,7 +68,7 @@ btnHold.addEventListener('click', function () {
     document.getElementById(`score--${activePlayer}`).textContent =
       scores[activePlayer];
     //2.Проверить игрока if >= 100;
-    if (scores[activePlayer] >= 100) {
+    if (scores[activePlayer] >= 20) {
       //Закончить игру
       diceEl.classList.add('hidden');
       playing = false;
@@ -73,3 +84,5 @@ btnHold.addEventListener('click', function () {
     }
   }
 });
+
+btnNew.addEventListener('click', init);
